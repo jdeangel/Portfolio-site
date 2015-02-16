cb247c74cb06c45ba09aeca294ced13435e1df65
jdeangel <jdeangel@mix.wvu.edu>
2015-02-16 10:44:16 -0500

    update before setting up sass/compass shell access

---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'compass';
+
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'scss'
+images_dir = 'images'
+javascripts_dir = 'js'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
