b17dcd0f1f8461f6dad2a45d25c2b19db5c6e8c2
jdeangel <jdeangel@mix.wvu.edu>
2015-02-16 15:53:09 -0500

    added mobile and bundler

--- config.rb
+++ config.rb
@@ -1,4 +1,15 @@
-require 'compass';
+require 'compass'
+require 'breakpoint'
+require 'singularitygs'
+require 'toolkit'
+require 'modular-scale'
+require 'color-schemer'
+
+require 'sassy-buttons'
+require 'autoprefixer-rails'
+require 'compass-recipes'
+require 'compass-normalize'
+require 'bootstrap-sass'
 
 preferred_syntax = :sass
 http_path = '/'
@@ -9,3 +20,5 @@ javascripts_dir = 'js'
 relative_assets = true
 line_comments = true
 # output_style = :compressed
+
+sourcemap = true
